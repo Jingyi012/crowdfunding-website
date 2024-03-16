59094ef6bc6d1256e230a9316f85784c7e862a58
jessLijie <wong.lijie1105@gmail.com>
2024-03-16 14:56:43 +0800

    updates

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../views/Dashboard.vue";
-import Tables from "../views/Tables.vue";
-import Billing from "../views/Billing.vue";
-import VirtualReality from "../views/VirtualReality.vue";
-import RTL from "../views/Rtl.vue";
-import Profile from "../views/Profile.vue";
-import Signup from "../views/Signup.vue";
-import Signin from "../views/Signin.vue";
 import AdminCrowdfundManage from "../views/AdminCrowdfundManage.vue";
-import UserManage from "../views/UserManage.vue";
-import Notification from "../views/Notification.vue";
-import TransactionList from "../views/TransactionList.vue";
-import Information from "../views/Information.vue";
+import Billing from "../views/Billing.vue";
 import createCampaign from "../views/CreateCampaign.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Information from "../views/Information.vue";
+import Notification from "../views/Notification.vue";
+import Profile from "../views/Profile.vue";
+import RTL from "../views/Rtl.vue";
+import Signin from "../views/Signin.vue";
+import Signup from "../views/Signup.vue";
+import Tables from "../views/Tables.vue";
+import TransactionList from "../views/TransactionList.vue";
+import UserManage from "../views/UserManage.vue";
+import VirtualReality from "../views/VirtualReality.vue";
 const routes = [
   {
     path: "/",
